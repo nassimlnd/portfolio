070dc76b054372780f1875bf2638234c3cd8eea3
Nassim LOUNADI <nassimlnd37@gmail.com>
2024-01-09 19:39:11 +0100

    build(css): css renamer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,20 @@
 import { defineConfig } from 'astro/config';
 
+import rename from 'astro-rename';
+import HashRenamer from './src/lib/hash-renamer';
+
 import tailwind from "@astrojs/tailwind";
+
+const renamer = new HashRenamer();
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+      tailwind(),
+      rename({
+          rename: {
+              strategy: (key) => renamer.rename(key),
+          }
+      })
+  ]
 });
